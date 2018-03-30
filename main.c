e5a0231f93e4ece4fba3f831bd928da4fb10875a
Kevin1er <vocanson.kevin1@gmail.com>
2018-03-30 06:18:38 -0700

    Branche dev

--- main.c
+++ main.c
@@ -1,98 +1,16 @@
 #include "GL/gl.h"
 #include "GL/glut.h"
 
-double angle = 0.0;
-double inc = .5;
-
-void Animer()
-{
-	angle += inc;
-	if(angle > 360) angle = 0;
-	if(angle < 0) angle = 360;
-
-	glutPostRedisplay();
-}
-
-void GererClavier(unsigned char touche, int x, int y)
-{
-	if(touche == 'a') inc = -inc;
-}
-
 void trace_cube()
 {
 	glBegin(GL_QUADS);
 
 	//Face avant
 	glColor3f(1.0,0.0,0.0);
-	glVertex3f(0,4,0);
-	glVertex3f(8,4,0);
-	glVertex3f(8,4,4);
-	glVertex3f(0,4,4);
-
-	//Face arriere
-	glColor3f(0.0,1.0,0.0);
 	glVertex3f(0,0,0);
-	glVertex3f(8,0,0);
-	glVertex3f(8,0,4);
-	glVertex3f(0,0,4);
-
-	//Face gauche
-	glColor3f(0.0,0.0,1.0);
-	glVertex3f(0,4,0);
-	glVertex3f(0,0,0);
-	glVertex3f(0,0,4);
-	glVertex3f(0,4,4);
-
-	//Face droite
-	glColor3f(1.0,1.0,0.0);
-	glVertex3f(8,4,0);
-	glVertex3f(8,0,0);
-	glVertex3f(8,0,4);
-	glVertex3f(8,4,4);
-
-	//Face haut
-	glColor3f(1.0,0.0,1.0);
-	glVertex3f(0,4,4);
-	glVertex3f(8,4,4);
-	glVertex3f(8,0,4);
-	glVertex3f(0,0,4);
-
-	//Face bas
-	glColor3f(0.0,1.0,1.0);
-	glVertex3f(0,4,0);
-	glVertex3f(8,4,0);
-	glVertex3f(8,0,0);
-	glVertex3f(0,0,0);
-
-
-	//Toit gauche
-	glColor3f(0.0,1.0,1.0);
-	glVertex3f(0,0,4);
-	glVertex3f(0,4,4);
-	glVertex3f(2,2,6);
-	glVertex3f(2,2,6);
-
-	//Toit droite
-	glColor3f(0.0,1.0,0.0);
-	glVertex3f(8,0,4);
-	glVertex3f(8,4,4);
-	glVertex3f(6,2,6);
-	glVertex3f(6,2,6);
-
-
-	//Toit Avant
-	glColor3f(1.0,0.0,0.0);
-	glVertex3f(0,4,4);
-	glVertex3f(8,4,4);
-	glVertex3f(6,2,6);
-	glVertex3f(2,2,6);
-
-	//Toit Arriere
-	glColor3f(1.0,1.0,0.0);
-	glVertex3f(0,0,4);
-	glVertex3f(8,0,4);
-	glVertex3f(6,2,6);
-	glVertex3f(2,2,6);
+	glVertex3f(0,1000,0);
+	glVertex3f(1000,1000,0);
+	glVertex3f(0,1000,0);
 
 	glEnd();
 	glutSwapBuffers();
@@ -103,10 +21,8 @@ void affichage()
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glFrustum(-2,2,2,-2,5,300);
-	gluLookAt(20,20,20,0,0,0,20,20,0);
-
-	glRotatef(angle,0,0,1);
+	glFrustum(-1,1,1,-1,5,1000);
+	gluLookAt(0,0,20,500,500,0,0,0,1);
 
 	trace_cube();
 
@@ -122,8 +38,6 @@ int main(int argc, char *argv[])
   	glutCreateWindow("Fenetre");
   	glEnable(GL_DEPTH_TEST);
   	glutDisplayFunc(affichage);
-  	glutIdleFunc(Animer);
-  	glutKeyboardFunc(GererClavier);
 
   	glutMainLoop();
   	return 0;
